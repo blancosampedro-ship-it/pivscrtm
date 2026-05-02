d0f993c5fa42cfc4fd38dbb8da40447bc8b7c091
jjbs <twetter@gmail.com>
2026-05-02 20:31:46 +0200

    chore(deps): require livewire/volt

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\VoltServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdminPanelProvider::class,
+    VoltServiceProvider::class,
 ];
