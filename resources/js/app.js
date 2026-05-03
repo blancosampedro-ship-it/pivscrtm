c1da205fcbf3917fc10fe209b9c6ed5c8d849c52
jjbs <twetter@gmail.com>
2026-05-03 18:34:06 +0200

    feat: configure vite-plugin-pwa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateServiceWorker = registerSW({
+	onNeedRefresh() {
+		window.dispatchEvent(new CustomEvent('pwa:update-available'));
+	},
+});
+
+window.winfinPivUpdateServiceWorker = updateServiceWorker;
