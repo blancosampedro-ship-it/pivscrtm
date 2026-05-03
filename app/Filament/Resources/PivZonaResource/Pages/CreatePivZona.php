24f20ab41f9669d3dcb77f2e9ca9199068383acf
jjbs <twetter@gmail.com>
2026-05-03 20:15:12 +0200

    feat(filament): add PivZona resource

---
+++ app/Filament/Resources/PivZonaResource/Pages/CreatePivZona.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\PivZonaResource\Pages;
+
+use App\Filament\Resources\PivZonaResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePivZona extends CreateRecord
+{
+    protected static string $resource = PivZonaResource::class;
+}
---
+++ app/Filament/Resources/PivZonaResource/Pages/EditPivZona.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources\PivZonaResource\Pages;
+
+use App\Filament\Resources\PivZonaResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPivZona extends EditRecord
+{
+    protected static string $resource = PivZonaResource::class;
+}
